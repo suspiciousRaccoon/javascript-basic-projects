c2d0113084bd5fff795abcbcbc71e17a41ac5a30
suspiciousRaccoon <spablocabral@gmail.com>
2023-04-27 19:46:50 -0300

    Add 18th project

--- 18-numbers/starter/app.js
+++ 18-numbers/starter/app.js
@@ -1,1 +1,25 @@
-console.log('numbers project');
+spans = [...document.querySelectorAll('.number')];
+
+function updateCount(el) {
+  const value = parseInt(el.dataset.value); // data-value in DOM
+  const increment = Math.ceil(value / 1000); // rounds up, can change 1000 value
+  // console.log(value, increment);
+  let initialValue = 0;
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+
+    el.textContent = initialValue;
+
+    // stops the counter if its greater than value
+    if (initialValue > value) {
+      el.textContent = value; // just in case
+      clearInterval(increaseCount); // stops the counter, for performance mostly, functionality still be the same
+      return; // not needed but good practice
+    }
+  }, 1); // runs  each milisecond
+}
+
+spans.forEach((el) => {
+  updateCount(el);
+});
