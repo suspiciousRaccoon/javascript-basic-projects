dd1c51a551c41c3c9c4b402e4f96f5e6e7fc0ec5
suspiciousRaccoon <spablocabral@gmail.com>
2023-04-28 10:37:49 -0300

    Add flashbang-preventing darktheme

--- 19-dark-mode/starter/app.js
+++ 19-dark-mode/starter/app.js
@@ -1,1 +1,23 @@
-console.log('dark mode');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+  ToggleDark(); //stupidly simple, dont even have to remove the :root values
+});
+
+function ToggleDark() {
+  dark = document.documentElement.classList.toggle('dark-theme');
+  console.log(dark);
+  if (dark) {
+    localStorage.setItem('dark', true);
+  } else {
+    localStorage.clear();
+  }
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+  if (localStorage.getItem('dark')) {
+    document.documentElement.classList.add('dark-theme');
+  }
+  document.body.style.visibility = 'visible';
+  document.body.style.opacity = 1;
+});
