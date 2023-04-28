6d3f21b3a5a7444fb2ee74a50a3a08f49bd50932
suspiciousRaccoon <spablocabral@gmail.com>
2023-04-28 13:57:26 -0300

    Add dynamically generated DOM from data.js

--- 19-dark-mode/starter/app.js
+++ 19-dark-mode/starter/app.js
@@ -1,10 +1,30 @@
 const btn = document.querySelector('.btn');
+const articleContainer = document.querySelector('.articles');
 
 btn.addEventListener('click', () => {
   dark = document.documentElement.classList.toggle('dark-theme');
   dark ? localStorage.setItem('dark', true) : localStorage.clear();
   //stupidly simple, dont even have to remove the :root values
 });
+
+const articlesData = articles
+  .map((article) => {
+    tempVal = `
+    <article class='post'>
+      <h2>${article.title}</h2>
+      <div class='post-info'>
+        <span>${article.date}</span> <span>${article.length} min read</span>
+      </div>
+      <p>
+        ${article.snippet}
+      </p>
+    </article>
+    `;
+    return tempVal;
+  })
+  .join('');
+
+articleContainer.innerHTML = articlesData;
 
 window.addEventListener('DOMContentLoaded', function () {
   if (localStorage.getItem('dark')) {
