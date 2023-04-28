5d09cf98f66b256bd42bfbc75cef00decffa1383
suspiciousRaccoon <spablocabral@gmail.com>
2023-04-28 10:53:19 -0300

    Refactored darktheme code

--- 19-dark-mode/starter/app.js
+++ 19-dark-mode/starter/app.js
@@ -1,18 +1,10 @@
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', () => {
-  ToggleDark(); //stupidly simple, dont even have to remove the :root values
-});
-
-function ToggleDark() {
   dark = document.documentElement.classList.toggle('dark-theme');
-  console.log(dark);
-  if (dark) {
-    localStorage.setItem('dark', true);
-  } else {
-    localStorage.clear();
-  }
-}
+  dark ? localStorage.setItem('dark', true) : localStorage.clear();
+  //stupidly simple, dont even have to remove the :root values
+});
 
 window.addEventListener('DOMContentLoaded', function () {
   if (localStorage.getItem('dark')) {
