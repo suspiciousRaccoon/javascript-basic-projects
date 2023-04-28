639da19cc6fd2747d5aea7ca02c70a2895a9718b
suspiciousRaccoon <spablocabral@gmail.com>
2023-04-28 19:46:13 -0300

    add textfiler, add filter buttons to DOM

--- 20-filters/starter/app.js
+++ 20-filters/starter/app.js
@@ -1,1 +1,61 @@
-console.log('filters project');
+const productsContainer = document.querySelector('.products-container');
+
+function displayProducts(productsParam) {
+  if (productsParam < 1) {
+    productsContainer.innerHTML = '<h4>No items match</h4>';
+    console.log(productsContainer.innerHTML);
+    return;
+  }
+
+  productsContainer.innerHTML = productsParam
+    .map((product) => {
+      return `
+      <article class='product' data-id='${product.id}'>
+        <img
+          src='${product.image}'
+          class='product-img img'
+          alt='${product.title}'
+        />
+        <footer>
+          <h5 class='product-name'>${product.title}</h5>
+          <span class='product-price'>${product.price}</span>
+        </footer>
+      </article>`;
+    })
+    .join('');
+}
+
+// text filter
+const form = document.querySelector('.input-form');
+const searchInput = document.querySelector('.search-input');
+
+form.addEventListener('keyup', () => {
+  filteredProducts = products.filter((product) => {
+    return product.title.toLowerCase().includes(searchInput.value);
+  });
+  displayProducts(filteredProducts);
+});
+
+// filter buttons
+
+const companyBtns = document.querySelector('.companies');
+
+function displayButtons() {
+  companyBtns.innerHTML = [
+    'all',
+    ...new Set(
+      Array.from(products, (product) => {
+        return product.company;
+      })
+    ),
+  ]
+    .map((company) => {
+      return `<button class="company-btn">${company}</button>`;
+    })
+    .join('');
+}
+
+window.addEventListener('load', () => {
+  displayProducts(products);
+  displayButtons();
+});
