380d41c6596a893ee65cdf26c342ef948d669e23
suspiciousRaccoon <spablocabral@gmail.com>
2023-05-29 19:28:16 -0300

    Add 27th project

--- 27-pagination/setup/app.js
+++ 27-pagination/setup/app.js
@@ -1,4 +1,44 @@
-import fetchFollowers from './fetchFollowers.js'
-import displayFollowers from './displayFollowers.js'
-import paginate from './paginate.js'
-import displayButtons from './displayButtons.js'
+import fetchFollowers from './fetchFollowers.js';
+import displayFollowers from './displayFollowers.js';
+import paginate from './paginate.js';
+import displayButtons from './displayButtons.js';
+
+const title = document.querySelector('.section-title h1');
+const btnContainer = document.querySelector('.btn-container');
+
+let index = 0;
+let pages = [];
+
+const setupUI = () => {
+  displayFollowers(pages[index]);
+  displayButtons(btnContainer, pages, index);
+};
+
+const init = async () => {
+  const followers = await fetchFollowers();
+  // displayFollowers(paginate(followers));
+  title.textContent = 'pagination';
+  pages = paginate(followers);
+  console.log(pages);
+  setupUI();
+};
+
+btnContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('btn-container')) return;
+  else if (e.target.classList.contains('page-btn')) {
+    index = parseInt(e.target.dataset.index);
+  } else if (e.target.classList.contains('next-btn')) {
+    index++;
+    if (index > pages.length - 1) {
+      index = 0;
+    }
+  } else if (e.target.classList.contains('prev-btn')) {
+    index--;
+    if (index < 0) {
+      index = pages.length - 1;
+    }
+  }
+  setupUI();
+});
+
+window.addEventListener('load', init);
--- 27-pagination/setup/fetchFollowers.js
+++ 27-pagination/setup/fetchFollowers.js
@@ -1,5 +1,14 @@
-const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
+const url = 'https://api.github.com/users/john-smilga/followers?per_page=100';
 
-const fetchFollowers = async () => {}
+const fetchFollowers = async () => {
+  try {
+    const response = await fetch(url);
+    const data = response.json();
+    return data;
+  } catch (error) {
+    alert(`There was an error ${error}`);
+    console.log(error);
+  }
+};
 
-export default fetchFollowers
+export default fetchFollowers;
